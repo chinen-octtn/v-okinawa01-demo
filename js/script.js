61ce73694228aef85f753f34c94951904138cc1a
chinen <octtn8c@gmail.com>
2021-03-19 14:26:06 +0900

    add: Sample

--- js/script.js
+++ js/script.js
@@ -2,7 +2,13 @@ var app = new Vue({
   el: '#app',
 
   data: {
-    message: '',
-    listData: ['リスト1', 'リスト2', 'リスト3', 'リスト4']
+    message: 0,
+    // listData: ['リスト1', 'リスト2', 'リスト3', 'リスト4']
+  },
+
+  methods: {
+    countUp: function() {
+      this.message++;
+    }
   }
 });
